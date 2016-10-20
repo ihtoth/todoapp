37b3a964aeedf41ecbdc272dc7d045ff9ccd35ca
fmoor <frederick.c.moor.v@intel.com>
2016-10-20 07:44:08 -0700

    create todo list

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+class ToDoItem(object):
+    def __init__(self, text, active=True):
+        self.text = text
+        self.active = active
+
+
+todo_list = [
+    ToDoItem('feed the fish'),
+    ToDoItem('clean the house', active=False),
+    ToDoItem('win the lottery'),
+]
+
+
 @app.route('/')
 def index():
-    return 'hello world!'
+    return render_template('list.html', title='To Do List', rows=todo_list)
 
 
 if __name__ == '__main__':
